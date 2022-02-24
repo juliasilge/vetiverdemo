716674fdff19bb2981d8c80b1bddde233b6dd688
Julia Silge <julia.silge@gmail.com>
2022-02-24 15:32:21 -0700

    How far I've gotten as of today

--- biv-svm/train_sagemaker.R
+++ biv-svm/train_sagemaker.R
@@ -23,14 +23,15 @@ library(paws)
 library(glue)
 library(pins)
 
-bucket <- glue("sagemaker-vetiver-", ids::adjective_animal(style = "kebab"))
+identifier <- glue::glue("vetiver-", ids::adjective_animal(style = "kebab"))
+identifier <- as.character(identifier)
 svc <- s3()
 svc$create_bucket(
-    Bucket = bucket,
+    Bucket = identifier,
     CreateBucketConfiguration = list(LocationConstraint = "us-east-2")
 )
 
-model_board <- board_s3(bucket = bucket, prefix = "pins/")
+model_board <- board_s3(bucket = identifier, prefix = "pins/")
 vetiver_pin_write(model_board, v)
 
 ## again!
@@ -39,7 +40,15 @@ vetiver_pin_write(model_board, v)
 ## again!!
 vetiver_pin_write(model_board, v)
 
-library(plumber)
-pr() %>%
-    vetiver_api(v, type = "class", debug = TRUE) %>%
-    pr_run(port = 8088)
+vetiver_write_plumber(
+    model_board,
+    "biv_svm",
+    type = "class",
+    debug = TRUE,
+    file = "biv-svm/plumber.R"
+)
+vetiver_write_docker(v, "biv-svm/plumber.R", "biv-svm/")
+
+container_registry <- ecr()
+container_registry$create_repository(repositoryName = identifier)
+container_registry$describe_images(repositoryName = identifier)
