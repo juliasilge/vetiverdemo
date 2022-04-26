7c43d974b8d8c764b233f112f2ced872aa41312d
Julia Silge <julia.silge@gmail.com>
2022-04-26 11:32:48 -0600

    Show how to `predict()` on Connect for private APIs

--- sacramento-rf/predict-rf.R
+++ sacramento-rf/predict-rf.R
@@ -4,8 +4,10 @@ library(vetiver)
 endpoint <- vetiver_endpoint("https://colorado.rstudio.com/rsc/sac_rf_api/predict")
 endpoint
 
+data(Sacramento, package = "modeldata")
 new_sac <- Sacramento %>%
     slice_sample(n = 20) %>%
     select(type, sqft, beds, baths)
 
-predict(endpoint, new_sac)
+apiKey <- Sys.getenv("CONNECT_API_KEY")
+predict(endpoint, new_sac, httr::add_headers(Authorization = paste("Key", apiKey)))
